405ec9b4a039dfe766f3b1f86c80596843aec01c
Otto Lampila <otto.lampila@packagemedia.fi>
2016-08-01 12:00:50 +0300

    Added 'IGNORE_IF_IP_STARTS_WITH' option

--- django2ban/backend.py
+++ django2ban/backend.py
@@ -1,9 +1,12 @@
 import logging
+from django.conf import settings
 from django.contrib.auth.models import User
 from .middlewares import ThreadLocal
 
-
 logger = logging.getLogger(__name__)
+
+IGNORE_IF_IP_STARTS_WITH = getattr(settings, 'D2B_IGNORE_IF_IP_STARTS_WITH',
+                                   ['172', '127'])
 
 
 class InvalidLoginBackend(object):
@@ -11,6 +14,8 @@ class InvalidLoginBackend(object):
         request = ThreadLocal.get_current_request()
         if request:
             ip = request.META['REMOTE_ADDR']
+            if ip.split('.')[0] in IGNORE_IF_IP_STARTS_WITH:
+                return None
             msg = u'Failed password for user %s from %s' % (username, ip)
             logger.info(msg)
 
